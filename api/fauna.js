da634bb33d0ceefb22808afc787ac72b62443358
HAINES-DESKTOP\richa <richardkhaines@hotmail.co.uk>
2020-05-11 11:19:25 +0200

    moved config, confirmed working api

--- api/fauna.js
+++
@@ -1,5 +0,0 @@
-const {client, query} = require('./_utils/db');
-
-module.exports = (req, res) => {
-    res.json({ name: 'John', email: 'john@example.com' })
-}
---
+++ api/graphql.js
@@ -0,0 +1,31 @@
+const {faunaServer} = require('./config');
+const {createHttpLink } = require('apollo-link-http');
+const { ApolloServer, makeRemoteExecutableSchema, introspectSchema } = require('apollo-server-micro');
+const fetch = require('isomorphic-fetch');
+
+const link = createHttpLink({
+    uri: 'https://graphql.fauna.com/graphql',
+    fetch,
+    headers: {
+      Authorization: `Bearer ${faunaServer}`,
+    },
+  })
+
+  const schema = makeRemoteExecutableSchema({
+    schema: introspectSchema(link),
+    link,
+  })
+
+const server = new ApolloServer({
+    schema,
+    playground: true,
+    introspection: true
+});
+
+module.exports = (req, res, ...args) => {
+    if (req.method === "OPTIONS") return res.status(200).send();
+  
+    const handler = server.createHandler();
+  
+    return handler(req, res, ...args);
+  };
